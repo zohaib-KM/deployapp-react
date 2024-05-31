613890b4463801cd1faa25acc06f0f44af9c9cfc
zohaib-KM <zohai.hamdani@kryptomind.com>
2024-05-31 18:22:33 +0500

    amlpify

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> This is new reactapp</h1>
+        <h1> This is reactapp deployed by AMPLIFY</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
